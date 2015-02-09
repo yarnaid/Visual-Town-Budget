d0c82cc4e8a3ae3b3db17ba9e4b02e9adbd11237
yarnaid <yarnaid@gmail.com>
2015-02-09 13:40:59 +0300

    summary chart added

--- src/httpdocs/includes/years_chart.php
+++ src/httpdocs/includes/years_chart.php
@@ -1,31 +1,32 @@
 <style type="text/css">
-    #years-chart svg {
+    #years-chart, #sum-years-chart svg {
       height: 400px;
   }
 </style>
 
 <div class="container">
     <div class="row-fluid">
-        <div class="dropdown" id="university-selector">
-            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                <span class="sr-only">Universities</span>
-                <span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu" role="menu">
-                <!-- <li role="presentation"><a role="menuitem">CMU</a></li> -->
-                <!-- <li role="presentation"><a role="menuitem">EMU</a></li> -->
-                <!-- <li role="presentation"><a role="menuitem">GVSU</a></li> -->
-            </ul>
+        <div>
+            <div class="dropdown" id="university-selector">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <span class="sr-only">Universities</span>
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                </ul>
+            </div>
+            <div id="year-selector" data-toggle="buttons" class="col-sm-12 btn-group btn-group-justified" role="group" aria-label="...">
+            </div>
+            <div id="years-chart">
+                <svg></svg>
+            </div>
         </div>
-        <div id="year-selector" data-toggle="buttons" class="btn-group" role="group" aria-label="...">
-            <!-- <button type="button" class="btn btn-default activ">Revenues</button> -->
-            <!-- <button type="button" class="btn btn-default">Expenditures</button> -->
-            <!-- <button type="button" class="btn btn-default">FacultyStaffComp</button> -->
-            <!-- <button type="button" class="btn btn-default">Enrollment</button> -->
-            <!-- <button type="button" class="btn btn-default">Size</button> -->
-        </div>
-        <div id="years-chart">
-            <svg></svg>
+        <div>
+            <div id="sum-years-selector" data-toggle="buttons" class="btn-group btn-group-justified" role="group" aria-label="...">
+            </div>
+            <div id="sum-years-chart">
+                <svg></svg>
+            </div>
         </div>
     </div>
 </div>
--- src/httpdocs/js/years_chart.js
+++ src/httpdocs/js/years_chart.js
@@ -1,3 +1,18 @@
+function format_shorten(value) {
+    if (value === undefined) {
+        return "N/A";
+    } else if (value >= 1000000) {
+        return "$" + Math.round(value / 1000000).toString() + " M";
+    } else if (value < 1000000 && value >= 1000) {
+        return "$" + Math.round(value / 1000).toString() + " K";
+    } else if (value < 1 && value != 0) {
+        return "¢" + Math.round(value * 100).toString();
+    } else {
+        return value.toString();
+    }
+}
+
+
 d3.json('/data/mpu-years/mpu-1-years.json', function(error, data){
   var btn_group = document.getElementById('year-selector');
   var dropdown_menu = document.getElementById('university-selector');
@@ -28,6 +43,7 @@ d3.json('/data/mpu-years/mpu-1-years.json', function(error, data){
     var sub_data = data[university];
     nv.addGraph(function() {
       var chart = nv.models.stackedAreaChart()
+      .interpolate('monotone')
       .x(function(d) { return d[0] })
       .y(function(d) { return d[1] })
       .clipEdge(true)
@@ -36,10 +52,12 @@ d3.json('/data/mpu-years/mpu-1-years.json', function(error, data){
 
       chart.xAxis
       .showMaxMin(false)
-      .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+      .tickFormat(function(d) { return d3.time.format('%Y')(new Date(d)) });
 
       chart.yAxis
-      .tickFormat(d3.format(',9.0f'));
+      .showMaxMin(false)
+      .ticks(4)
+      .tickFormat(function(d) { return format_shorten(d);});
 
       d3.select('#years-chart svg')
       .datum(sub_data[category])
@@ -53,15 +71,71 @@ d3.json('/data/mpu-years/mpu-1-years.json', function(error, data){
   updateData();
 
   $('#year-selector button').click(function() {
-      current_category = $(this).text();
-      $(this).addClass('active').siblings().removeClass('active');
-      updateData(category=current_category);
-    });
+    current_category = $(this).text();
+    $(this).addClass('active').siblings().removeClass('active');
+    updateData(category=current_category);
+  });
 
   $('#university-selector li').click(function() {
     current_university = jQuery('a', this).text();
     $(this).addClass('active').siblings().removeClass('active');
     updateData(university=current_university);
+  });
+
+});
+
+
+
+d3.json('/data/mpu-years/mpu-sum.json', function(error, data){
+  var btn_group = document.getElementById('sum-years-selector');
+  current_sum_category = 'Revenues';
+
+  for (category in data) {
+    var b = document.createElement('button');
+    var btn = $(b);
+    btn.addClass('btn btn-default');
+    b.type = 'button';
+    btn.text(category);
+    btn_group.appendChild(b);
+  };
+
+  function updateData (category) {
+    category = current_sum_category;
+    nv.addGraph(function() {
+      var chart = nv.models.stackedAreaChart()
+      .interpolate('monotone')
+      .x(function(d) { return d[0] })
+      .y(function(d) { return d[1] })
+      .clipEdge(true)
+      .useInteractiveGuideline(true)
+      ;
+
+      chart.xAxis
+      .showMaxMin(false)
+      .tickFormat(function(d) { return d3.time.format('%Y')(new Date(d)) });
+
+      chart.yAxis
+      .ticks(4)
+      .showMaxMin(false)
+      .tickFormat(function (d) {
+                return format_shorten(d);
+            });
+
+      d3.select('#sum-years-chart svg')
+      .datum(data[category])
+      .transition().duration(500).call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
+  };
+  updateData();
+
+  $('#sum-years-selector button').click(function() {
+    current_sum_category = $(this).text();
+    $(this).addClass('active').siblings().removeClass('active');
+    updateData(category=current_sum_category);
   });
 
 });
